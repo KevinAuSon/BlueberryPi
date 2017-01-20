ec9ca3cdc9f51182ea6dffa281d43f57932ed2bf
KevinAuSon <raisingsmile@gmail.com>
2017-01-20 15:48:04 +0000

    Add systemctl service

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-#install python-dbus dbus-glib python-gobject
+sudo pacman -S -y python-dbus dbus-glib python-gobject
 
 sudo mv blueberry /usr/lib
 sudo mv bin/blueberry /usr/bin
 sudo chmod +x /usr/bin/blueberry
+
+sudo mv blueberry.service /etc/systemd/system/
+sudo systemctl enable blueberry.service
+sudo systemctl start blueberry.service
